7d998efa6da2bc69dbb70b94682b2fda4a2afc7b
barrywype <root@imie-dev-debian>
2014-10-08 16:57:49 +0200

    thegrid

--- src/yinac/morpion/view/TheGrid.java
+++ src/yinac/morpion/view/TheGrid.java
@@ -8,25 +8,43 @@ package yinac.morpion.view;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Stroke;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JComponent;
+import yinac.morpion.model.IMorpionModel;
 
 /**
  *
  * @author imie
  */
 public class TheGrid extends JComponent {
+    
+    public Plateau plateau;
+    private IMorpionModel imorpmod;
+    
+    public TheGrid(Plateau plateau, IMorpionModel imorpmod){
+        this.plateau = plateau;
+        this.imorpmod = imorpmod;
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+        }});
+        
+    }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(Color.red);
+        Integer cellWidth = getWidth()/3;
+        Integer cellHeight = getHeight()/3;
+                
+                
         for (int cpt = 0; cpt < 3; cpt++) {
             for (int cpt2 = 0; cpt2 < 3; cpt2++) {
-                g2d.drawRect(cpt * 50, cpt * 50, cpt2 * 50, cpt2 * 50);
+                g2d.drawRect(cpt * cellWidth, cpt2 * cellHeight, cellWidth, cellHeight);
             }
         }
-
     }
-
 }
