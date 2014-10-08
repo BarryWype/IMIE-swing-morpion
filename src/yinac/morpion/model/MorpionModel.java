0893cc8867a55850e7af10606258add90b58f57e
oKcerG <gr3cko@gmail.com>
2014-10-08 15:32:11 +0200

    Added dummy game logic in model

--- src/yinac/morpion/model/MorpionModel.java
+++ src/yinac/morpion/model/MorpionModel.java
@@ -3,25 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package yinac.morpion.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
 import java.util.Random;
-
+import yinac.morpion.model.events.CellChangeEvent;
+import yinac.morpion.model.events.NextPlayerChangeEvent;
+import yinac.morpion.model.events.StateChangeEvent;
 
 /**
  *
  * @author imie
  */
-public class MorpionModel implements IMorpionModel {
+public class MorpionModel extends Observable implements IMorpionModel {
+
     private Player crossPlayer;
     private Player circlePlayer;
     private Player winningPlayer;
     private Cell nextCellType;
     private GameState state;
     private List<Cell> cells;
-    
+
     MorpionModel(Player player1, Player player2) {
         if (new Random().nextBoolean()) {
             crossPlayer = player1;
@@ -33,13 +37,38 @@ public class MorpionModel implements IMorpionModel {
         nextCellType = Cell.circle;
         winningPlayer = null;
         state = GameState.running;
-        while (cells.size() < getRowCount() * getColumnCount())
+        cells = new ArrayList<Cell>();
+        while (cells.size() < getRowCount() * getColumnCount()) {
             cells.add(Cell.empty);
+        }
+    }
+
+    public void playCell(int row, int column) {
+        cells.set(getIndexFromRowColumn(row, column), nextCellType);
+        setChanged();
+        notifyObservers(new CellChangeEvent(row, column));
+        checkGameState();
+        nextCellType = toggleCellType(nextCellType);
+        setChanged();
+        notifyObservers(new NextPlayerChangeEvent());
+    }
+
+    private void checkGameState() {
+        if (new Random().nextInt(4) == 0) { // 1 chance sur 4
+            if (new Random().nextBoolean()) {
+                winningPlayer = new Random().nextBoolean() ? crossPlayer : circlePlayer;
+                state = GameState.won;
+            } else {
+                state = GameState.won;
+            }
+            setChanged();
+            notifyObservers(new StateChangeEvent());
+        }
     }
 
     @Override
     public Cell getCellAt(int row, int column) {
-        return cells.get(row + column*getRowCount());
+        return cells.get(getIndexFromRowColumn(row, column));
     }
 
     @Override
@@ -64,8 +93,12 @@ public class MorpionModel implements IMorpionModel {
 
     @Override
     public Cell getCellTypeFromPlayer(Player player) {
-        if (player == crossPlayer) return Cell.cross;
-        if (player == circlePlayer) return Cell.circle;
+        if (player == crossPlayer) {
+            return Cell.cross;
+        }
+        if (player == circlePlayer) {
+            return Cell.circle;
+        }
         return Cell.empty;
     }
 
@@ -83,5 +116,13 @@ public class MorpionModel implements IMorpionModel {
     public int getColumnCount() {
         return 3;
     }
-    
+
+    private static Cell toggleCellType(Cell cellType) {
+        return cellType == Cell.circle ? Cell.cross : Cell.circle;
+    }
+
+    private int getIndexFromRowColumn(int row, int column) {
+        return row + column * getRowCount();
+    }
+
 }
